edfe413b45a5281bc39dde7adf0dbb32c1956d26
Brice <bricel67@gmail.com>
2022-10-03 11:33:50 +0200

    Create new note and new view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,9 +8,21 @@ class NotesController < ApplicationController
   def show
   end
 
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+  end
+
   private
 
   def set_note
     @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content)
   end
 end
