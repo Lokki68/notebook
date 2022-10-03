81e026c401f300adca1172eb581e3ab5294b6f23
Brice <bricel67@gmail.com>
2022-10-03 12:16:02 +0200

    Add user

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,7 @@
 class NotesController < ApplicationController
   before_action :set_note, only: %i[show edit update destroy]
+  before_action :authenticate_user!
+
 
   def index
     @notes = Note.all
@@ -52,6 +54,6 @@ class NotesController < ApplicationController
   end
 
   def note_params
-    params.require(:note).permit(:title, :content)
+    params.require(:note).permit(:title, :content, :user_id)
   end
 end
