d93aac910b63b0035959359c662c617dcede1a57
Brice <bricel67@gmail.com>
2022-10-03 11:27:22 +0200

    Create show method and show view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,16 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: %i[show]
 
   def index
     @notes = Note.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
   end
 end
